07acc40a7b9a7160ce6b74371a0684cf3ab3e0f6
Артем Трегубчак <Artprofi91@MacBook-Pro-Artem.local>
2017-09-23 19:21:24 -0500

    files

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+
+var friends = [
+  {
+    name: "Daniel Hernandez",
+    photo: "https://avatars3.githubusercontent.com/u/28367077?v=4&s=400",
+    scores: [
+      "5",
+      "1",
+      "1",
+      "5",
+      "1",
+      "5",
+      "5",
+      "2",
+      "1",
+      "3"
+    ]
+  },
+  {
+    name: "Latisha McNeel",
+    photo: "https://avatars3.githubusercontent.com/u/2047327?v=4&s=400",
+    scores: [
+      "5",
+      "2",
+      "3",
+      "5",
+      "3",
+      "4",
+      "5",
+      "3",
+      "4",
+      "3"
+    ]
+  },
+  {
+    name: "Kathryn Herod",
+    photo: "https://avatars3.githubusercontent.com/u/7751467?v=4&s=400",
+    scores: [
+      "5",
+      "3",
+      "5",
+      "3",
+      "2",
+      "5",
+      "5",
+      "3",
+      "5",
+      "3"
+    ]
+  },
+  {
+    name: "Jacob Thomas",
+    photo: "https://avatars3.githubusercontent.com/u/14240957?v=4&s=400",
+    scores: [
+      "5",
+      "2",
+      "1",
+      "5",
+      "3",
+      "5",
+      "4",
+      "2",
+      "3",
+      "5"
+    ]
+  },
+  {
+    name: "Camden Kirkland",
+    photo: "https://avatars1.githubusercontent.com/u/6783131?v=4&s=400",
+    scores: [
+      "5",
+      "2",
+      "1",
+      "4",
+      "5",
+      "5",
+      "5",
+      "2",
+      "2",
+      "4"
+    ] 
+  }
+];
+
+module.exports = friends;
